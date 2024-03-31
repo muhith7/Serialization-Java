b228fc78df950d76e47891372a3e05f500f28fbd
muhith7 <abdulmuhithnawawi@gmail.com>
2024-04-01 00:06:33 +0700

    Serialisasi Produk

---
+++ src/main/java/com/mycompany/p03/serialization/P03Serialization.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p03.serialization;
+
+/**
+ *
+ * @author abdul
+ */
+public class P03Serialization {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/serialisasi/MainSetting.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package serialisasi;
+
+/**
+ *
+ * @author abdul
+ */
+public class MainSetting {
+    
+    Main kemana kita hari ini sayangg ?? :)
+    
+}
